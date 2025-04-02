495f3d5a65754cbc09ac5d72c884ebaab384dd32
Karolvi25 <karolarana07@gmail.com>
2025-04-02 08:09:57 -0500

    array php

---
+++ array.php
@@ -0,0 +1,20 @@
+package com.mycompany.funtion_java_johann_tatiana;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Funtion_Java_Johann_Tatiana {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(45, 78);
+        System.out.println(resultado);
+        
+    }
+    public static int suma(int a, int b){
+    
+    return a + b;
+}
+    
+}
